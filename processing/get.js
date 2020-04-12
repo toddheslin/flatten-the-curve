696e8b484619f82c4bb38ada5152defba9108783
Todd Heslin <todd@beingremarkable.me>
2020-04-12 14:43:35 +1200

    Removing q in favour of native Promise.all

--- processing/get.js
+++ processing/get.js
@@ -1,7 +1,6 @@
 const csv = require('csv-parser')
 const fs = require('fs')
 const _ = require('lodash')
-const Q = require('q')
 const request = require('request')
 const { parse, format } = require('date-fns')
 const NZAdvanced = require('./get-nz-advanced')
@@ -11,20 +10,20 @@ const createFiles = (output_folder) => {
   let deaths = []
   let recovered = []
   let population_data = []
-  
+
   let us_data = {}
   let us_cum = {}
   let nz_data = {}
 
   let country_array = []
   let cumulative = {}
-  
-  Q.all([
+
+  Promise.all([
     new Promise( (resolve) => {
       request('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
       .pipe(csv())
       .on('data', data => { confirmed.push(data) })
-      .on('end', () => {  
+      .on('end', () => {
         resolve()
       })
     }),
@@ -32,7 +31,7 @@ const createFiles = (output_folder) => {
       request('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
       .pipe(csv())
       .on('data', data => { deaths.push(data) })
-      .on('end', () => {    
+      .on('end', () => {
         resolve()
       })
     }),
@@ -40,7 +39,7 @@ const createFiles = (output_folder) => {
       request('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
       .pipe(csv())
       .on('data', data => { recovered.push(data) })
-      .on('end', () => {    
+      .on('end', () => {
         resolve()
       })
     }),
@@ -48,12 +47,12 @@ const createFiles = (output_folder) => {
       fs.createReadStream('data/population_world_bank.csv')
       .pipe(csv())
       .on('data', data => { population_data.push(data) })
-      .on('end', () => {  
-        resolve()  
+      .on('end', () => {
+        resolve()
       })
     }),
     new Promise( (resolve, reject) => {
-      request('https://covidtracking.com/api/states/daily', (err, _, body) => {    
+      request('https://covidtracking.com/api/states/daily', (err, _, body) => {
         if (err) reject(err);
         us_data = getUnitedStates(JSON.parse(body));
         us_cum = getCumulatives(us_data.states)
@@ -69,30 +68,30 @@ const createFiles = (output_folder) => {
     })
   ])
   .then(() => {
-    countries = merge_object( 
-      restructure_inputs(confirmed), 
+    countries = merge_object(
+      restructure_inputs(confirmed),
       restructure_inputs(deaths),
       restructure_inputs(recovered),
       'Country/Region'
     )
-    
+
     // Remove the empty time series data
     _.forEach(countries, c => {
       c.time_series = c.time_series.filter( (t) => t.confirmed != 0 )
     })
-    
-    // convert date strings to actual dates 
+
+    // convert date strings to actual dates
     _.forEach(countries, c => {
       c.time_series.map( t => {
         t.date = format(parse(t.date, 'MM/dd/yy', new Date()),'yyyy-MM-dd') + 'T00:00:00.000Z'
       })
     })
-    
+
     // Relabel country names
     _.forEach(countries, c => {
-      c.name = 
-        c.name == 'US'            ? 'United States' : 
-        c.name == 'Korea, South'  ? 'South Korea' : 
+      c.name =
+        c.name == 'US'            ? 'United States' :
+        c.name == 'Korea, South'  ? 'South Korea' :
         c.name == 'Czechia'       ? 'Czech Republic' :
         c.name
     })
@@ -106,14 +105,14 @@ const createFiles = (output_folder) => {
       country => country.name != 'United States' && country.name != 'New Zealand'
     )
 
-    
+
     const canada = getCountry( confirmed, deaths, recovered, population_data,
       r => (
-        r['Country/Region'] == 'Canada' && 
-        r['Province/State'] != 'Recovered' && 
+        r['Country/Region'] == 'Canada' &&
+        r['Province/State'] != 'Recovered' &&
         r['Province/State'] != 'Diamond Princess' &&
         r['Province/State'] != 'Grand Princess'
-      ), 
+      ),
       'canada'
     )
 
@@ -126,12 +125,12 @@ const createFiles = (output_folder) => {
       r => r['Country/Region'] == 'China',
       'china'
     )
-    
+
     country_array.push(us_data.total_only)
     country_array.push(nz_data)
-    
 
-      
+
+
     const advanced_countries = [
       {
         name: 'United States',
@@ -158,8 +157,8 @@ const createFiles = (output_folder) => {
         cum:  china.cum
       }
     ]
-    
-  
+
+
     country_array= country_array.filter(c => c.population > 1000000 && c.highest_confirmed > 10)
 
     country_array.forEach( c=> {
@@ -176,31 +175,30 @@ const createFiles = (output_folder) => {
     fs.writeFile(output_folder + '/countries.json', JSON.stringify(country_array , null, 2), function(err) {
       if(err) return console.log(err);
       console.log("Country file was saved!");
-    }); 
+    });
     fs.writeFile(output_folder + '/cumulative.json', JSON.stringify(cumulative, null, 2), function(err) {
       if(err) return console.log(err);
       console.log("Cumulative was saved!");
-    }); 
+    });
     fs.writeFile(output_folder + '/advanced.json', JSON.stringify(advanced_countries, null, 2), function(err) {
       if(err) return console.log(err);
       console.log("Advanced JSON saved!");
     });
-    
-    
-    
+
+
+
   })
-  
+
   .catch( (err) => {
     console.log(err)
   })
-  .done()
-    
-        
-        
-    
-        
-        
-        
+
+
+
+
+
+
+
 }
 
 /*
@@ -227,9 +225,9 @@ const restructure_region = (confirmed, deaths, recovered, population) => {
 
   const provinces = merge_object(confirmed, deaths, recovered, 'Province/State')
   province_array = _.map(provinces, p => p)
-  
+
   const total_time_series = {}
-  
+
   province_array.forEach(p => {
     p.time_series.forEach(day => {
       day.old_date = day.date
@@ -246,7 +244,7 @@ const restructure_region = (confirmed, deaths, recovered, population) => {
 
   const time_series_array = _.map(total_time_series, day => day)
   const most_recent_day = time_series_array[time_series_array.length -1]
-  
+
   const total = {
     name: "All",
     population: population,
@@ -267,9 +265,9 @@ const restructure_region = (confirmed, deaths, recovered, population) => {
   province_array.forEach( p => {
     p.time_series = p.time_series.filter( t => t.confirmed != 0 )
   })
-  
+
   return province_array
-  
+
 }
 
 
@@ -284,7 +282,7 @@ const add_population_data = (areas, population_data) => {
             time.confirmed_per_mil = time.confirmed / (data.population / 1000000)
           if(time.deaths)
             time.deaths_per_mil = time.deaths / (data.population / 1000000)
-        })      
+        })
       }
     })
   })
@@ -293,14 +291,14 @@ const add_population_data = (areas, population_data) => {
 
 const merge_object = (confirmed, deaths, recovered, field_to_use) => {
   let combined = {}
-  
+
   confirmed.forEach( area => {
-    let name = area[field_to_use] 
+    let name = area[field_to_use]
     highest = 0
     time_series = []
     _.forEach(area, (val, key) => {
       if(validKey(key)){
-        time_series.push( { 
+        time_series.push( {
           date: key, confirmed: parseInt(val)
         } )
         if(val > highest)
@@ -326,7 +324,7 @@ const merge_object = (confirmed, deaths, recovered, field_to_use) => {
           combined[name].highest_deaths = highest
 
         }
-        
+
       }
     })
   })
@@ -347,7 +345,7 @@ const merge_object = (confirmed, deaths, recovered, field_to_use) => {
           combined[name].highest_recovered = highest
 
         }
-        
+
       }
     })
   })
@@ -372,7 +370,7 @@ const restructure_inputs = (all_countries) => {
     }
 
   })
-  
+
   countries_multiple_column_names = seen_countries.filter( (c) => c.count > 1 ).map( c => c.name)
 
   countries = all_countries.filter( c => ! countries_multiple_column_names.includes(c) )
@@ -380,7 +378,7 @@ const restructure_inputs = (all_countries) => {
   countries_multiple_column_names.forEach( name => {
     countries.push(combine_regions(all_countries.filter( (result) => result['Country/Region'] == name)))
   })
-  
+
   return countries.filter(remove_negatives)
 }
 
@@ -389,9 +387,9 @@ const combine_regions = (initial_regions) => {
   initial_regions.forEach(region => {
   _.forEach(region, (val, key) => {
       if(validKey(key)){
-        if( combined.hasOwnProperty(key) ) 
+        if( combined.hasOwnProperty(key) )
           combined[key] += parseInt(val)
-        else 
+        else
           combined[key] = parseInt(val)
       }
       else if(key == 'Country/Region')
@@ -405,9 +403,9 @@ const remove_negatives = (area) => {
   let found_case = false
   _.forEach(area, (val, key) => {
     if(validKey(key))
-      if( parseInt(val) != 0) 
-        found_case = true 
-    
+      if( parseInt(val) != 0)
+        found_case = true
+
   })
   return found_case
 }
@@ -422,14 +420,14 @@ const getCumulatives = (areas) => {
 
   const confirmed_ranges = [100, 250, 500, 1000, 5000]
   const death_ranges = [10, 50, 100, 250, 500]
-  // The structure needs to e 
+  // The structure needs to e
   // We get an array of countries
-  
+
   areas.forEach(area => {
-    
+
     const confirmed = []
     const deaths = []
-    
+
     confirmed_ranges.forEach(range => {
       let count_of_days = 0
       const confirmed_for_range = []
@@ -470,7 +468,7 @@ const getCumulatives = (areas) => {
     })
 
     // Either array is populated. Then append area to cumulative output
-    if(confirmed.length || deaths.length){ 
+    if(confirmed.length || deaths.length){
       const area_to_append = {
         highest_confirmed: area.highest_confirmed,
         highest_deaths: area.highest_deaths,
@@ -480,10 +478,10 @@ const getCumulatives = (areas) => {
 
       if(confirmed.length) area_to_append.confirmed = confirmed
       if(deaths.length) area_to_append.deaths = deaths
-      
+
       output_areas.push(area_to_append)
     }
-    
+
   })
 
   return output_areas
@@ -573,14 +571,14 @@ const getUnitedStates = (json_data) => {
           recovered: day.recovered || 0
         })),
         highest_confirmed,
-        highest_deaths, 
+        highest_deaths,
         highest_hospitalized,
         highest_tests,
         highest_recovered
       })
     })
 
-    
+
 
 
     const total_time_series = {}
@@ -602,7 +600,7 @@ const getUnitedStates = (json_data) => {
 
     const US_time_series = _.map(total_time_series, day => day)
     const most_recent_day = US_time_series[US_time_series.length -1]
-    
+
     const total = {
       name: "United States",
       population: 327167434,
@@ -623,7 +621,7 @@ const getUnitedStates = (json_data) => {
     })
 
     states.push(total)
-    
+
     return {
       states,
       total_only: total
@@ -632,8 +630,9 @@ const getUnitedStates = (json_data) => {
 
 
 
-if(process.argv.length == 3 ) createFiles(process.argv[2])
-else{
+if(process.argv.length == 3 ) {
+createFiles(process.argv[2])
+} else{
   console.log("Whoops!Usage:\nnode get.js path_to_output_folder")
   getUnitedStates()
 }
